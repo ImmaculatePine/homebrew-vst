f9541e5b4b0b9859e6481fd29037414659293eb0
Aleksandr Borovykh <aleksandr.borovykh@gmail.com>
2025-09-11 20:35:58 +0200

    Add mixwave-spiritbox-mike-stringer cask (#3)

---
+++ Casks/mixwave-spiritbox-mike-stringer.rb
@@ -0,0 +1,17 @@
+cask "mixwave-spiritbox-mike-stringer" do
+  version "1.0.3"
+  sha256 "f75967746ffdab309a27129dc745c280136a895d052345dec3ad086f380c3d93"
+
+  url "https://mixwave-downloads.s3.us-east-1.amazonaws.com/plugins/Spiritbox-Mike-Stringer/MixWave+Spiritbox+-+Mike+Stringer+v#{version}.pkg",
+      verified: "mixwave-downloads.s3.us-east-1.amazonaws.com/"
+  name "MixWave: Mike Stringer"
+  desc "Heaviest plugin from the guitarist of Spiritbox: Mike Stringer"
+  homepage "https://mixwave.com/products/spiritbox-mike-stringer"
+
+  depends_on macos: ">= :high_sierra"
+  depends_on cask: "ilok-license-manager"
+
+  pkg "MixWave+Spiritbox+-+Mike+Stringer+v#{version}.pkg"
+
+  uninstall pkgutil: "net.mixwave.pkg.MixWaveSpiritbox-MikeStringer*"
+end
