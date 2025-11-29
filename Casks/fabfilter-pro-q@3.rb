12e9bd51c8101678cdbfa8dbb295e797fafe8f23
Aleksandr Borovykh <aleksandr.borovykh@gmail.com>
2025-11-29 15:56:39 +0100

    Add FabFilter Pro-Q 3 (#6)

---
+++ Casks/fabfilter-pro-q@3.rb
@@ -0,0 +1,20 @@
+cask "fabfilter-pro-q@3" do
+  version "3.27"
+  sha256 "bcd1fdff8d9e2f4fbb662f89d7ecde1cbbf48c4bd0ab18bda9cf33af77c5b708"
+
+  url "https://cdn-b.fabfilter.com/downloads/ffproq#{version.no_dots}.dmg"
+  name "FabFilter Pro-Q"
+  desc "Equaliser plug-in"
+  homepage "https://www.fabfilter.com/products/pro-q-3-equalizer-plug-in"
+
+  livecheck do
+    url "https://www.fabfilter.com/support/downloads"
+    regex(/Pro-Q.*?v?(\d+(?:\.\d+)+)/im)
+  end
+
+  pkg "FabFilter Pro-Q #{version} Installer.pkg"
+
+  uninstall pkgutil: "com.fabfilter.Pro-Q.#{version.major}"
+
+  # No zap stanza required
+end
