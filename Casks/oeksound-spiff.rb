580b23873777af55666496b1a0aa547b03c9a867
Aleksandr Borovykh <aleksandr.borovykh@gmail.com>
2025-11-29 13:52:40 +0100

    Add oeksound spiff (#4)

---
+++ Casks/oeksound-spiff.rb
@@ -0,0 +1,16 @@
+cask "oeksound-spiff" do
+  version "1.4.4"
+  sha256 "5f3149bbe3157969596537538bf3e1b4d4389250449782e700b1a613a10a04c8"
+
+  url "https://oeksound.ams3.cdn.digitaloceanspaces.com/spiff_v#{version.delete(".")}_Mac.pkg",
+      verified: "oeksound.ams3.cdn.digitaloceanspaces.com/"
+  name "oeksound Spiff"
+  desc "Adaptive transient processor plugin"
+  homepage "https://oeksound.com/plugins/spiff/"
+
+  depends_on cask: "ilok-license-manager"
+
+  pkg "spiff_v#{version.delete(".")}_Mac.pkg"
+
+  uninstall pkgutil: "com.oeksound.spiff*"
+end
