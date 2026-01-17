ef6bdcc8f41b45c01d0ebc8f225959829b40d815
Aleksandr Borovykh <aleksandr.borovykh@gmail.com>
2026-01-17 23:10:48 +0100

    Add Mixwawe Manuel Gardner Fernandes

---
+++ Casks/mixwave-manuel-gardner-fernandes.rb
@@ -0,0 +1,16 @@
+cask "mixwave-manuel-gardner-fernandes" do
+  version "1.0.0"
+  sha256 "985a4c5f76bf75fcdce811ecdd0f3fa5b5144538ade13b7a04d758829358bab8"
+
+  url "https://mixwave-downloads.s3.us-east-1.amazonaws.com/plugins/Manuel-Gardner-Fernandes/MixWave+Manuel+Gardner+Fernandes+v#{version}.pkg",
+      verified: "mixwave-downloads.s3.us-east-1.amazonaws.com/"
+  name "MixWave: Manuel Gardner Fernandes"
+  desc "Versatile and expressive, capturing the essence of Manuel's evolving sound"
+  homepage "https://mixwave.com/products/manuel-gardner-fernandes"
+
+  depends_on cask: "ilok-license-manager"
+
+  pkg "MixWave+Manuel+Gardner+Fernandes+v#{version}.pkg"
+
+  uninstall pkgutil: "com.mixwave.pkg.MixWaveManuelGardnerFernandes*"
+end
