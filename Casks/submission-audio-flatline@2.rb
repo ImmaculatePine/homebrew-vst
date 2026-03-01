24948750eae582315f3332aa0013ced4c48e73bf
Aleksandr Borovykh <aleksandr.borovykh@gmail.com>
2026-03-01 12:48:54 +0100

    Add SubMission Audio Flatline 2 (#15)

---
+++ Casks/submission-audio-flatline@2.rb
@@ -0,0 +1,15 @@
+cask "submission-audio-flatline@2" do
+  version "2.0.6"
+  sha256 "e634504c3618cd5252ea07f9e123be6bfbf53370e45486e9002148a732c196e4"
+
+  url "https://downloads.submissionaudio.com/Flatline/Flatline+#{version}/Flatline+#{version}.pkg"
+  name "SubMission Audio Flatline 2"
+  desc "Clipper, limiter and intelligent maximizer"
+  homepage "https://www.submissionaudio.com/products/flatline"
+
+  depends_on cask: "ilok-license-manager"
+
+  pkg "Flatline #{version}.pkg"
+
+  uninstall pkgutil: "com.submissionaudio.flatline2.*"
+end
