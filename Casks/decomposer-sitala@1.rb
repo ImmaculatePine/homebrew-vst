4a5eaffac74d96f07c9455c8c499acc911fba81c
Aleksandr Borovykh <aleksandr.borovykh@gmail.com>
2026-04-17 00:13:11 +0200

    Add Decomposer Sitala 1.0 (#20)

---
+++ Casks/decomposer-sitala@1.rb
@@ -0,0 +1,22 @@
+cask "decomposer-sitala@1" do
+  version "1.0"
+  sha256 "d07a7c107a1d944bb2fc11392bec9c606e1c9caf357db6bd979e248f6c584831"
+
+  url "https://decomposer.de/sitala/releases/Sitala-#{version}.dmg"
+  name "Sitala"
+  desc "Drum sampler plugin"
+  homepage "https://decomposer.de/sitala/"
+
+  pkg "Install Plug-ins.pkg"
+
+  uninstall pkgutil: "de.decomposer.Sitala.*"
+
+  zap trash: [
+    "~/Library/Application Support/Sitala",
+    "~/Library/Caches/de.decomposer.Sitala",
+    "~/Library/Preferences/de.decomposer.Sitala.plist",
+    "~/Library/Preferences/Sitala.settings",
+    "~/Library/Preferences/SitalaStandalone.settings",
+    "~/Library/Saved Application State/de.decomposer.Sitala.savedState",
+  ]
+end
